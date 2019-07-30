2071409e4313a303f4db966655a0e65cde0344c0
Max Strålin <max.stralin@ce-check.eu>
2019-07-30 20:20:37 +0200

    Attribute created

---
+++ src/Core/Attributes/JsClassConverterAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSharpToJs.Core.Interfaces;
+
+namespace CSharpToJs.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class JsClassConverterAttribute : Attribute
+    {
+        public IJsClassConverter ClassConverter { get; }
+
+        public JsClassConverterAttribute(Type classConverter)
+        {
+            if (!typeof(IJsClassConverter).IsAssignableFrom(classConverter))
+            {
+                throw new ArgumentException("Type provided is not an implementation of IJsPropertyConverter", nameof(classConverter));
+            }
+
+            ClassConverter = Activator.CreateInstance(classConverter) as IJsClassConverter;
+        }
+    }
+}
