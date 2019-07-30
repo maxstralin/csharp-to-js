3c2108327bf846f30b73c960347e77623d9529b2
Max Strålin <max.stralin@ce-check.eu>
2019-07-30 18:40:20 +0200

    Tests

--- src/Core/Services/OutputPathResolver.cs
+++ src/Core/Services/OutputPathResolver.cs
@@ -12,7 +12,7 @@ namespace CSharpToJs.Core.Services
         public string Resolve(OutputPathContext context)
         {
             //TODO: .NET Standard 2.1 has Linq.SkipLast(1);
-            var path = context.JsClass.OriginalType.FullName.Replace($"{context.ProcessingNamespace}.", string.Empty).Split('.').ToList();
+            var path = context.JsClass.OriginalType.FullName?.Replace($"{context.ProcessingNamespace}.", string.Empty).Split('.').ToList() ?? throw new ArgumentNullException(nameof(context.JsClass.OriginalType.FullName));
 
             var pathParts = Enumerable.Empty<string>().Append(context.Config.OutputPath).Append(context.AssemblyDetails.SubFolder ?? string.Empty).Concat(path.Take(path.Count - 1))
                 .Append(context.AssemblyDetails.SubFolder).Append($"{context.JsClass.Name}.js");
--- tests/CSharpToJs.Tests/JsPropertyConverterTests.cs
+++ tests/CSharpToJs.Tests/JsPropertyConverterTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using CSharpToJs.Core.Attributes;
+using CSharpToJs.Core.Interfaces;
 using CSharpToJs.Core.Models;
 using CSharpToJs.Core.Services;
 using CSharpToJs.Tests.Mocks;
@@ -145,6 +148,26 @@ namespace CSharpToJs.Tests
             Assert.Equal(JsPropertyType.Plain, jsProp.PropertyType);
             Assert.Equal(jsProp.Value,
                 JsonConvert.SerializeObject(new IgnoredClass(), Formatting.None, Settings.SerializerSettings));
+        }
+
+        [Fact]
+        public void JsPropertyConverterAttribute()
+        {
+            var mock = new CustomPropertyConverterClass();
+            var propInfo = mock.GetType().GetProperty(nameof(mock.MyProperty));
+            var converterContext = new PropertyConverterContext();
+
+            var (expectedName, expectedValue) = ("Custom", "Super");
+            var attribute = propInfo.GetCustomAttribute<JsPropertyConverterAttribute>();
+            var customConverter = attribute.PropertyConverter;
+            var converterInstance = Activator.CreateInstance(customConverter.GetType()) as IJsPropertyConverter;
+            var propResult = converterInstance?.Convert(converterContext);
+
+
+            Assert.NotNull(customConverter);
+            Assert.NotNull(propResult);
+            Assert.Equal(expectedName, propResult.Name);
+            Assert.Equal(expectedValue, propResult.Value);
         }
     }
 }
